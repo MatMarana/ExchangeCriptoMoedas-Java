0d5d5e8de2a15d4b1408165f72cb8d1358a7b241
mateus marana <assumateus@gmail.com>
2024-05-23 14:58:40 -0300

    first commit

---
+++ src/exchangecriptomoedas/ExchangeCriptoMoedas.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exchangecriptomoedas;
+import View.TelaBemVindo;
+
+/**
+ *
+ * @author unifmassuena
+ */
+public class ExchangeCriptoMoedas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaBemVindo tela = new TelaBemVindo();
+        tela.setVisible(true);
+        
+    }
+    
+}
